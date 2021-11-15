96662de1662b317345bbb9bfe55435851f41001e
zhsssy <893118851@qq.com>
2021-11-15 13:27:25 +0800

    :sparkles::bug:

---
+++ include/lexical_analysis.h
@@ -0,0 +1,149 @@
+//
+// Created by zhsssy on 2021.11.15.
+//
+
+#ifndef COMPILER_PRINCIPLE_EXPERIMENT_LEXICAL_ANALYSIS_H
+#define COMPILER_PRINCIPLE_EXPERIMENT_LEXICAL_ANALYSIS_H
+
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+enum LEXICAL_RESULT {
+    ERROR = -1,
+    IDENTIFICATION = 0,
+    DIGIT,
+    OCT,
+    HEX,
+    KEY,
+    END
+};
+
+
+class LexicalAnalysisMachine {
+private:
+    ifstream fin;
+    std::string tmp;
+    std::string token;
+    const char *p;
+    LEXICAL_RESULT result;
+    vector<std::string> keyword;
+
+public:
+    explicit LexicalAnalysisMachine(const std::string &filename)
+            : fin(filename, ios_base::in), p(nullptr), result(ERROR) {
+        keyword = {"do",
+                   "while"
+                   "if",
+                   "then",
+                   "else",
+        };
+    }
+
+    bool next() {
+        skip_blank();
+
+        if (p == nullptr || *p == 0) {
+            result = END;
+            token = "#";
+            return false;
+        }
+
+        tokenInit();
+
+        if (isalpha(*p)) {
+            add();
+            while (isalnum(*p))
+                add();
+
+            if (iskeyword()) result = KEY;
+            else result = IDENTIFICATION;
+        } else if (*p == '0') {
+            add();
+            if (*p == 'x' || *p == 'X') {
+                add();
+                if (is_hex(*p)) {
+                    add();
+                    while (is_hex(*p))
+                        add();
+                    result = HEX;
+                } else {
+                    add();
+                    result = ERROR;
+                }
+            } else if (isdigit(*p)) {
+                if (*p != '0' && *p != '8' && *p != '9') {
+                    add();
+                    while (is_oct(*p))
+                        add();
+                    result = OCT;
+                } else {
+                    add();
+                    result = ERROR;
+                }
+            } else result = DIGIT;
+        } else if (isdigit(*p)) {
+            add();
+            while (isdigit(*p))
+                add();
+            result = DIGIT;
+        } else add(), result = KEY;
+
+        return true;
+    }
+
+    LEXICAL_RESULT get_result() const {
+        return result;
+    }
+
+    std::string get_token() const {
+        return token;
+    }
+
+
+    bool is_open() const {
+        return fin.is_open();
+    }
+
+private:
+    void skip_blank() {
+        if (p != nullptr)
+            while (*p < 33 && *p > 0) ++p;
+
+        while ((p == nullptr || *p == 0) && getline(fin, tmp)) {
+            if (tmp.empty()) continue;
+            tmp.erase(0, tmp.find_first_not_of(' '));
+            if (tmp.empty()) continue;
+            tmp.erase(tmp.find_last_not_of(' ') + 1);
+
+            p = tmp.c_str();
+
+            while (*p < 33 && *p > 0) ++p;
+        }
+    }
+
+    void tokenInit() {
+        token.erase(token.cbegin(), token.cend());
+    }
+
+    inline void add() {
+        token += *p++;
+    }
+
+    bool iskeyword() const {
+        return find(keyword.begin(), keyword.end(), get_token()) != keyword.end();
+    }
+
+    static inline bool is_oct(const char ch) {
+        return ch < '8' && ch >= '0';
+    }
+
+    static inline bool is_hex(const char ch) {
+        return (ch <= '9' && ch > '1') || (ch <= 'f' && ch >= 'a');
+    }
+};
+
+
+#endif //COMPILER_PRINCIPLE_EXPERIMENT_LEXICAL_ANALYSIS_H
---
+++ src/lexical_analysis.cpp
@@ -0,0 +1,42 @@
+// Created by zhsssy on 2021.11.15.
+//
+
+#include "../include/lexical_analysis.h"
+
+#include <iostream>
+#include <string>
+
+
+int main(int argc, char *argv[]) {
+    string token, s;
+    LEXICAL_RESULT result;
+
+    LexicalAnalysisMachine l("../resource/output.txt");
+
+    if (!l.is_open()) {
+        cout << "Can't open " << argv[1] << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    while (l.next()) {
+        result = l.get_result();
+        switch (result) {
+            case IDENTIFICATION:
+            case DIGIT:
+            case OCT:
+            case HEX:
+                cout << "<" << result << ", " << l.get_token() << ">" << endl;
+                break;
+            case KEY:
+                cout << "<" << l.get_token() << ", " << "->" << endl;
+                break;
+            case ERROR:
+                cout << "ERROR" << endl;
+                exit(EXIT_FAILURE);
+            default:
+                break;
+        }
+    }
+}
+
+
